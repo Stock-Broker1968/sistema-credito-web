72fb894eab5ace064ab9ee6f6640a9088d5fb9a2
Stock-Broker1968 <azpiriroberto68@gmail.com>
2025-07-27 15:42:12 -0600

    Update app.py
    Sistema actualizado

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ def encriptar_nip(nip):
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('login.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
